5f1623058a3221e915f36158fd959704b51ccd52
vBoBv <vannponhvath@gmail.com>
2020-09-10 15:08:27 +1200

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+    }
+  }
+}
